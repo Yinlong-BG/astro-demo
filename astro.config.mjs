14a8b685c2cf86769a004f068ab06dc92dfcdd41
Yinlong <yl.bestgift@gmail.com>
2025-07-04 20:30:44 +0200

    config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://yinlong-bg.github.io/a', // 替换为你的 GitHub Pages 地址
+    base: '/astro-demo', // 替换为你的仓库名（如果不是用户/组织页面，需设置）
+    output: 'static', // 确保输出为静态文件
+});
