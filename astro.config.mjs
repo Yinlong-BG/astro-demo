81d289ef5083016c8de293d18e22510d97c8a8de
Yinlong <yl.bestgift@gmail.com>
2025-07-07 13:07:22 +0200

    npx astro add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yinlong-bg.github.io/', // 替换为你的 GitHub Pages 地址
-    base: '/astro-demo', // 替换为你的仓库名（如果不是用户/组织页面，需设置）
-    output: 'static', // 确保输出为静态文件
+  // 替换为你的 GitHub Pages 地址
+  site: 'https://yinlong-bg.github.io/',
+
+  // 替换为你的仓库名（如果不是用户/组织页面，需设置）
+  base: '/astro-demo',
+
+  // 确保输出为静态文件
+  output: 'static',
+
+  integrations: [preact()]
 });
